6a21a2fe122ab456ac2a3105c36ba8e003527615
Christophe Beveraggi <christophe@beve.org>
2020-11-08 16:34:53 +0100

    Homepage wip

--- wp-content/themes/arkeogis/front-page.php
+++ wp-content/themes/arkeogis/front-page.php
@@ -16,12 +16,86 @@ get_header();
 ?>
 
 	<main id="primary" class="site-main">
+
 		<div class="landing-block">
-			<h1><?php pll_e("Cartographier,  conserver,  étudier, gérer…");?></h1>
-			<h2><?php pll_e("ArkeoGIS, partage et utilisation de données spatialisées sur le passé");?></h2>
-			<div class="button-container">
-				<a href="" target="_blank"><?php pll_e("Ouvrir l’application");?></a>
-				<a href="" target="_blank"><?php pll_e("Créer un compte");?></a>
+			<div class="main-container">
+				<h1><?php pll_e("Cartographier,  conserver,  étudier, gérer…");?></h1>
+				<h2><?php pll_e("ArkeoGIS, partage et utilisation de données spatialisées sur le passé");?></h2>
+				<div class="button-container">
+					<a href="" target="_blank"><?php pll_e("Ouvrir l’application");?></a>
+					<a href="" target="_blank"><?php pll_e("Créer un compte");?></a>
+				</div>
+			</div>
+			<div class="logos-container">
+				<img class="strasbourg" src="<?php echo get_template_directory_uri().'/assets/images/logos/Universite_de_Strasbourg.jpg';?>" alt="Université de Strasbourg"/>
+				<img class="archimede" src="<?php echo get_template_directory_uri().'/assets/images/logos/Archimed.jpg';?>" alt="Archimede"/>
+				<img class="misha" src="<?php echo get_template_directory_uri().'/assets/images/logos/Misha.jpg';?>" alt="Misha"/>
+				<img class="masa" src="<?php echo get_template_directory_uri().'/assets/images/logos/Masa.jpg';?>" alt="Masa"/>
+				<img class="humanum" src="<?php echo get_template_directory_uri().'/assets/images/logos/Humanum.png';?>" alt="Humanum"/>
+			</div>
+		</div>
+
+		<div class="platform-block">
+			<div class="background"></div>
+			<div class="gradient"></div>
+			<h2><?php pll_e("Plateforme de publication <b>de données scientifiques unifiées</b>");?></h2>
+			<div class="video-container">
+				<div class="dots-container">
+					<div></div>
+					<div></div>
+					<div></div>
+				</div>
+			</div>
+			<div class="specs-container">
+				<div class="spec">
+					<h3>
+						<?php pll_e("Importation  Qualifiée");?>
+						<div class="num">01</div>
+					</h3>
+					<div class="text"><?php pll_e("Importer vos données guidé par l’assistance à la correction en ligne, ajouter les méta-données (DublinCore) et obtenir des données inter-opérable.");?>
+					</div>
+					<a href="<?php the_permalink(pll_get_post(10));?>" alt="<?php pll_e("Importation  Qualifiée");?>"></a>
+				</div>
+				<div class="spec">
+					<h3>
+						<?php pll_e("Cartographie Interactive");?>
+						<div class="num">02</div>
+					</h3>
+					<div class="text"><?php pll_e("Quoi ? où ? quand ? par qui ? Découvrir et interroger des datasets spatialisés pluridiciplinaires, pour un état de l’art de la recherche des données partagés...");?></div>
+					<a href="<?php the_permalink(pll_get_post(10));?>" alt="<?php pll_e("Cartographie Interactive");?>"></a>
+				</div>
+				<div class="spec">
+					<h3>
+						<?php pll_e("Exportation Normalisée");?>
+						<div class="num">03</div>
+					</h3>
+					<div class="text"><?php pll_e("Exporter le résultat de votre recherche dans un format normalisé (CSV) pour le ré-utiliser directement dans les outils de votre choix tableur, QGIS, R...");?></div>
+					<a href="<?php the_permalink(pll_get_post(10));?>" alt="<?php pll_e("Exportation Normalisée");?>"></a>
+				</div>
+				<div class="spec">
+					<h3>
+						<?php pll_e("Personnaliser");?>
+						<div class="num">04</div>
+					</h3>
+					<div class="text"><?php pll_e("Un espace personnalisé, pour choisir dans l’ensemble des données mises en commun en 4 langues vos aire, chronologie, langue, bases de données et vos termes de travail");?></div>
+					<a href="<?php the_permalink(pll_get_post(10));?>" alt="<?php pll_e("Personnaliser");?>"></a>
+				</div>
+				<div class="spec">
+					<h3>
+						<?php pll_e("Diffuser");?>
+						<div class="num">05</div>
+					</h3>
+					<div class="text"><?php pll_e("Diffuser le niveau d’information de votre choix, et rendez le accessible dans la communautée d’utilisateurs qualifiés en indiquant comment vous citer");?></div>
+					<a href="<?php the_permalink(pll_get_post(10));?>" alt="<?php pll_e("Diffuser");?>"></a>
+				</div>
+				<div class="spec">
+					<h3>
+						<?php pll_e("Pérenniser");?>
+						<div class="num">06</div>
+					</h3>
+					<div class="text"><?php pll_e("Des identifiants pérennes DOI pour le stokage, des ARK (Archival Resource Key) pour les termes utilisés, un hébergement sécurisé et dupliqué pour une conservation à long terme");?></div>
+					<a href="<?php the_permalink(pll_get_post(10));?>" alt="<?php pll_e("Pérenniser");?>"></a>
+				</div>
 			</div>
 		</div>
 	</main><!-- #main -->
--- wp-content/themes/arkeogis/inc/translations.php
+++ wp-content/themes/arkeogis/inc/translations.php
@@ -3,3 +3,16 @@
   pll_register_string("partage et utilisation", "ArkeoGIS, partage et utilisation de données spatialisées sur le passé", 'Arkeogis', false);
   pll_register_string("ouvrir application", "Ouvrir l’application", 'Arkeogis', false);
   pll_register_string("ouvrir un compte", "Créer un compte", 'Arkeogis', false);
+  pll_register_string("plateforme de données", "Plateforme de publication <b>de données scientifiques unifiées</b>", 'Arkeogis', false);
+  pll_register_string("importation qualifiée", "Importation  Qualifiée", 'Arkeogis', false);
+  pll_register_string("importer vos données", "Importer vos données guidé par l’assistance à la correction en ligne, ajouter les méta-données (DublinCore) et obtenir des données inter-opérable.", 'Arkeogis', true);
+  pll_register_string("cartographie interractive", "Cartographie Interactive", 'Arkeogis', false);
+  pll_register_string("quoi, ou", "Quoi ? où ? quand ? par qui ? Découvrir et interroger des datasets spatialisés pluridiciplinaires, pour un état de l’art de la recherche des données partagés...", 'Arkeogis', true);
+  pll_register_string("exportation normalisée", "Exportation Normalisée", 'Arkeogis', false);
+  pll_register_string("exporter le résultat", "Exporter le résultat de votre recherche dans un format normalisé (CSV) pour le ré-utiliser directement dans les outils de votre choix tableur, QGIS, R...", 'Arkeogis', true);
+  pll_register_string("personnaliser", "Personnaliser", 'Arkeogis', false);
+  pll_register_string("un espace personnalisé", "Un espace personnalisé, pour choisir dans l’ensemble des données mises en commun en 4 langues vos aire, chronologie, langue, bases de données et vos termes de travail", 'Arkeogis', true);
+  pll_register_string("diffuser", "Diffuser", 'Arkeogis', false);
+  pll_register_string("diffuser le niveau", "Diffuser le niveau d’information de votre choix, et rendez le accessible dans la communautée d’utilisateurs qualifiés en indiquant comment vous citer", 'Arkeogis', true);
+  pll_register_string("perenniser", "Pérenniser", 'Arkeogis', false);
+  pll_register_string("des identifiants perennes", "Des identifiants pérennes DOI pour le stokage, des ARK (Archival Resource Key) pour les termes utilisés, un hébergement sécurisé et dupliqué pour une conservation à long terme", 'Arkeogis', true);
